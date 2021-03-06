07858e8bb1d4acb7a952dd68336078aa2a88adc2
Angad Thandi <angadthandi8@gmail.com>
2021-03-05 22:09:20 -0700

    Setting up Notepad class

---
+++ src/modules/notepad/Notepad.java
@@ -0,0 +1,48 @@
+package modules.notepad;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Notepad {
+
+    private ArrayList<String> allContent;
+    private Stack<ArrayList<String>> undoStack;
+    private Stack<ArrayList<String>> redoStack;
+
+    public void Notepad() {
+
+    }
+
+    public void Display() {
+
+    }
+
+    public void Display(Integer m, Integer n) {
+
+    }
+
+    public boolean Delete(Integer n) {
+        return true;
+    }
+
+    public boolean Delete(Integer n, Integer m) {
+        return true;
+    }
+
+    public boolean Copy(Integer n, Integer m) {
+        return true;
+    }
+
+    public boolean Paste(Integer n) {
+        return true;
+    }
+
+    public boolean Undo() {
+        return true;
+    }
+
+    public boolean Redo() {
+        return true;
+    }
+
+}
